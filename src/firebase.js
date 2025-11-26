b116af265dfe6fa16fb2014b3a15426cdb8ff0e9
Mayur Patel <patel.mayur2113@gmail.com>
2025-11-26 23:30:27 +0530

    Add fallback values for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDzwxpAjnG5jKSsKPw5LuVm2W_aXGN5gOs",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "portfolio-8421e.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "portfolio-8421e",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "portfolio-8421e.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "995985846482",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:995985846482:web:66dd6c109b95489b88bd0b"
 };
 
 // Initialize Firebase
